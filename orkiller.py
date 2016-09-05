3770cfd956b36bc52a7ea0639c0a0a10316d43fc
rob <mistyhands@users.noreply.github.com>
2016-09-05 12:06:46 +0100

    detect gibberish, write exceptions to file

--- orkiller.py
+++ orkiller.py
@@ -1,4 +1,4 @@
-import praw, time, datetime, traceback, obot
+import praw, time, datetime, traceback, obot, re
 #obot is used to login with OAuth, you will get an exception if you attempt to import it
 
 """
@@ -13,6 +13,7 @@ recentlyDonePosts = []
 recentlyBannedUsers = []
 recentlyModdedSubs = []
 
+testMode = True
 botActive = True
 
 #get posts from /r/mod
@@ -21,6 +22,33 @@ rmod = r.get_subreddit("mod")
 
 #string sent to user
 sentToUserString = "You have been temporarily banned for using a comment overwrite script. This is considered spam. In future, please use a less spammy overwrite message. Thank you."
+
+def isSpam(string):
+        notanum = 0
+        caps = 0
+        spaces = 0
+        instring = string
+        for x in instring:
+            if bool(re.search("([^A-Za-z0-9 ])", x)):
+                notanum += 1
+            if bool(re.search("([A-Z])", x)):
+                caps += 1
+            if x == " ":
+                spaces += 1
+        if len(string) <20 and len(string) != 1:
+            return False
+        percent = (notanum / len(instring))*100
+        capsPercent = (caps / len(instring))*100
+        if percent >=20 and spaces/len(string) < 0.05 and len:
+            if "[](" in string:
+                return False
+            else:
+                if capsPercent > 30 and capsPercent < 50:
+                    return True
+                else:
+                    return False 
+        return False
+
 
 def is_banned(sub, user):
     if [i for i in list(r.get_banned(sub)) if i.name == user]:
@@ -38,25 +66,42 @@ def check():
                 text = comment.body
             except AttributeError:
                 text = comment.selftext
-            if scriptKeyword in text and comment.id not in recentlyDonePosts:
+            #set up some bools
+            overwriteMessageInText = scriptKeyword in text
+            itemNotDone = comment.id not in recentlyDonePosts
+            itemIsGibberish = isSpam(text)
+            either = overwriteMessageInText or itemIsGibberish
+            
+            if either and itemNotDone:
                 if comment.banned_by == None:
                         #string for mod note
                         subofcomment = comment.subreddit
                         modString = "Greasemonkey overwrite script:", comment.id
                         
+                        #don't try to ban deleted users
                         if comment.author == None:
                             cmtAuthor = "[deleted]"
                         else:
                             cmtAuthor = comment.author.name
+                        
                         #ban args
                         banargs = {"duration": 3, "note": modString, "ban_message": sentToUserString}
                         
-                        #remove the comment regardless
-                        comment.remove(spam=False)
-                        print("Removed id " + comment.id + " by " + cmtAuthor + " from /r/" + comment.subreddit.display_name)
-                        recentlyDonePosts.append(comment.id)
-                        #don't bother banning them if they're already banned
+                        #send item to spam filter if it's gibberish
+                        if(itemIsGibberish):
+                            if not testMode:
+                                comment.remove(spam=True)
+                            print("Removed id " + comment.id + " by " + cmtAuthor + " from /r/" + comment.subreddit.display_name + ". (gibberish comment)")
+                            print("        Text:", text)
+                        else:
+                            if not testMode:
+                                comment.remove(spam=False)
+                            print("Removed id " + comment.id + " by " + cmtAuthor + " from /r/" + comment.subreddit.display_name)
+                            print("        Text:", text)
+                        if not testMode:
+                            recentlyDonePosts.append(comment.id)
                         
+                        #don't bother banning them if they're already banned
                         try:
                             if is_banned(subofcomment, cmtAuthor) or cmtAuthor in recentlyBannedUsers:
                                 pass
@@ -64,8 +109,9 @@ def check():
                                 if cmtAuthor == "[deleted]":
                                     pass
                                 else:
-                                    rmod.add_ban(subofcomment, **banargs)
-                                    recentlyBannedUsers.append(cmtAuthor)
+                                    if not testMode:
+                                        rmod.add_ban(subofcomment, **banargs)
+                                        recentlyBannedUsers.append(cmtAuthor)
                                     print("Tempbanned " + cmtAuthor + " in " + "/r/" + comment.subreddit.display_name)
                         except:
                             recentlyBannedUsers.append(cmtAuthor)
@@ -76,11 +122,18 @@ def check():
         #let's relax a little
         time.sleep(1)
 def getTime():
-    currentSysTime = time.localtime()
-    return time.strftime('%m/%d/%y @ %H:%M:%S', currentSysTime)
+    return round(time.time()*1000)
+    
+if testMode:
+    print("/!\ TEST MODE! All actions are SIMULATED.")
 while True:
     try:
         check()
-    except:
+    except Exception as e:
+        fileName = str(getTime()) + ".txt"
+        print("exception occurred! sleeping 10 seconds. written to file")
+        exFile = open(fileName, 'w')
+        exFile.write(str(e))
+        exFile.close()
         time.sleep(10)
-
+        
